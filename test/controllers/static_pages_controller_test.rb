ff0a7ab7b0c9ccd628f7ae9515f0541c6fd1beab
Tracy Kaneshiro <tkaneshiro@gmail.com>
2016-02-25 21:31:34 -0800

    contact page exercise

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -24,5 +24,10 @@ class StaticPagesControllerTest < ActionController::TestCase
   	assert_select "title", "About | #{@base_title}"
   end
 
+  test "should get test" do
+  	get :contact
+  	assert_response :success
+  	assert_select "title", "Contact | #{@base_title}"
+  end
 
 end
