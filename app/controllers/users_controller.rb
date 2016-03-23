24c14dbfe72d52b1daa4bf00792e14488b4cec1c
Tracy Kaneshiro <tkaneshiro@gmail.com>
2016-03-23 09:28:32 -0700

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
 
 	def show
 		@user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
 	end
 
   def new
@@ -55,16 +56,7 @@ class UsersController < ApplicationController
 
     # before filters
 
-    # confirms a logged_in_user
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
-
-    def correct_user
+     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
     end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
+  resources :microposts,          only: [:create, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
